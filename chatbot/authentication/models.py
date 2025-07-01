2421909944959faebb8ad2f4e0288bb84702e129
CSamu7 <samuel.pdg@hotmail.com>
2025-06-30 23:37:20 -0600

    Se corrigieron varios errores de los modelos y se cambio el nombre de algunos campos.

--- chatbot/authentication/models.py
+++ chatbot/authentication/models.py
@@ -6,22 +6,25 @@ class User(AbstractBaseUser):
   username = models.CharField(max_length=50)
   email = models.EmailField(validators=[validators.EmailValidator()], unique=True)
 
+  USERNAME_FIELD = "email"
+  EMAIL_FIELD = "email"
+
   def __str__(self):
     return self.username
 
 class Chat(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE)
-  name = models.CharField(max_length=150)
+  title = models.CharField(max_length=150)
   created_at = models.DateTimeField(auto_now_add=True)
 
   def __str__(self):
-    return self.name
+    return self.title
 
 class Message(models.Model):
   chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
   content = models.TextField()
   send_at = models.DateTimeField(auto_now_add=True)
   
   def __str__(self):
-    return self.content[:50] if self.content > 50 else self.content
-
+    return self.content[:50] if len(self.content) > 50 else self.content
