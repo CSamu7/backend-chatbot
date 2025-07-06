c7d8fb7f3e9aca63bdea16ce36a8f517b22bdb29
CSamu7 <samuel.pdg@hotmail.com>
2025-07-05 23:55:42 -0600

    Se añadio una validacion para el Mensaje

--- chatbot/chat/serializers.py
+++ chatbot/chat/serializers.py
@@ -1,18 +1,21 @@
 from rest_framework import serializers
 from .models import Chat, Message
+from authentication.serializers import UsersSerializer
 
 class ChatSerializer(serializers.ModelSerializer):
   class Meta:
     model = Chat
-    fields = [
-      "id", 
-      "user", 
-      "title", 
-      "created_at"
-    ]
-    
+    fields = ["id", "title", "created_at"]
 
-class MessageSerializer(Message):
+class MessageSerializer(serializers.ModelSerializer):
+  user = UsersSerializer()
+
   class Meta:
     model = Message
-    fields = ["id", "chat", "user", "content", "send_at"]
+    fields = ["user", "text", "send_at"]
+
+  def validate_text(self, text = str):
+    if len(text) <= 0:
+      raise serializers.ValidationError("No puedes enviar un mensaje vacio")
+    
+    return text
