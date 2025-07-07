8423907fcaf5ad2ae2c2d5e582f067e70a2c93ee
CSamu7 <samuel.pdg@hotmail.com>
2025-07-06 23:10:45 -0600

    Se cambiaron las vistas función a vistas genericas

--- chatbot/authentication/urls.py
+++ chatbot/authentication/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from authentication import views
 
 urlpatterns = [
-  # path('auth/', views.UserAuth.as_view()),
-  path('user/', views.UserPost.as_view()),
-  path('user/<int:pk>', views.UserDetails.as_view())
+  path('users/<int:pk>', views.RetrieveDeleteUser.as_view()),
+  path('users/', views.PostUser.as_view())
 ]
--- chatbot/authentication/views.py
+++ chatbot/authentication/views.py
@@ -1,34 +1,10 @@
-from authentication.models import User
 from authentication.serializers import UsersSerializer
+from authentication.models import User
+from rest_framework import generics
 
-from rest_framework.views import APIView
-from rest_framework import mixins, generics, status
-from rest_framework.response import Response
-
-from rest_framework.authtoken.models import Token
-
-# class UserAuth(APIView):
-#   serializer_class = UsersSerializer
-
-#   def auth(self, request):
-#     email = request.data.email
-#     password = request.data.password
-
-#     try:
-#       user = User.objects.get(email=email, password=password)
-#       token = Token.objects.create(user)
-
-#       return Response(token)
-#     except:
-#       return Response(status=status.HTTP_401_UNAUTHORIZED)
-
-#Generico
-class UserPost(generics.GenericAPIView, mixins.CreateModelMixin):
+class PostUser(generics.CreateAPIView):
   serializer_class = UsersSerializer
 
-  def create(self, request, *args, **kwargs):
-    return self.list(request, *args, **kwargs)
-  
-class UserDetails(generics.RetrieveUpdateDestroyAPIView):
+class RetrieveDeleteUser(generics.RetrieveDestroyAPIView):
   queryset = User.objects.all()
   serializer_class = UsersSerializer
