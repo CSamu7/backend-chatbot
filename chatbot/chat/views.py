7224fdb700f9907d6e4e64a59832a231d7f4714c
CSamu7 <samuel.pdg@hotmail.com>
2025-07-08 18:38:01 -0600

    Se añadieron vistas genericas para el Chat y Mensaje.

--- chatbot/chat/views.py
+++ chatbot/chat/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
+from chat.serializers import ChatSerializer, MessageSerializer
+from chat.models import Chat, Message
+from rest_framework import generics
 # Create your views here.
+class ListPostChats(generics.ListCreateAPIView):
+  serializer_class = ChatSerializer
+
+  def get_queryset(self):
+    pk_user = self.kwargs['pk']
+    return Chat.objects.filter(user=pk_user)
+
+class RetrieveDeleteChat(generics.RetrieveDestroyAPIView):
+  queryset = Chat.objects.all()
+  serializer_class = ChatSerializer()
+
+class ListPostMessage(generics.ListCreateAPIView):
+  queryset = Message.objects.all()
+  serializer_class = MessageSerializer()
+
