aa88d4a30ed8ef25e1b33cdac67ad859604511a9
CSamu7 <samuel.pdg@hotmail.com>
2025-07-16 15:24:17 -0600

    Se dividio la lista y el post de chats.

---
+++ chatbot/chat/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from chat import views
+
+urlpatterns = [
+  path('users/<int:pk>/chats/', views.ListPostChats.as_view()),
+  path('chats/', views.PostChats.as_view()),
+  path('chats/<int:pk>', views.RetrieveDeleteChat.as_view()),
+  path('chats/<int:pk>/messages/', views.ListPostMessage.as_view()),
+]
--- chatbot/chat/views.py
+++ chatbot/chat/views.py
@@ -2,15 +2,19 @@ from chat.serializers import ChatSerializer, MessageSerializer
 from chat.models import Chat, Message
 from rest_framework import generics
 from .permissions import ChatPermissions
-# Create your views here.
-# Esto esta fallando!
-class ListPostChats(generics.ListCreateAPIView):
+from authentication.serializers import UsersSerializer
+from pprint import pprint
+
+class ListPostChats(generics.ListAPIView):
   serializer_class = ChatSerializer
   permission_classes = [ChatPermissions]
 
   def get_queryset(self):
     pk_user = self.kwargs['pk']
     return Chat.objects.filter(user=pk_user)
+  
+class PostChats(generics.CreateAPIView):
+  serializer_class = ChatSerializer
 
 class RetrieveDeleteChat(generics.RetrieveDestroyAPIView):
   queryset = Chat.objects.all()
