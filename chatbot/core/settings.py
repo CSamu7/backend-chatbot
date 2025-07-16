f6f4547d3255d0a50ba2cad5631bcc61cb28951c
CSamu7 <samuel.pdg@hotmail.com>
2025-07-16 15:53:46 -0600

    Se configuro el proyecto para que utilice JWT

--- chatbot/core/settings.py
+++ chatbot/core/settings.py
@@ -130,6 +130,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
       'rest_framework.permissions.IsAuthenticated'
     ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+      'rest_framework_simplejwt.authentication.JWTAuthentication'
+    ],
     'PAGE_SIZE': 100
 }
 
