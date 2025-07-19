3a9e7de3e37e83c8239288aae30f12f59c51ebf6
CSamu7 <samuel.pdg@hotmail.com>
2025-07-18 22:31:12 -0600

    Se creo una vista personalizada del Token para desabilitar la autenticación global

--- chatbot/authentication/urls.py
+++ chatbot/authentication/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from authentication import views
-from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
   path('users/<int:pk>', views.RetrieveDeleteUser.as_view()),
   path('users/', views.PostUser.as_view()),
-  path('users/auth', TokenObtainPairView.as_view(), name="token_obtain"),
+  path('users/auth', views.MyTokenView.as_view(), name="token_obtain"),
   path('users/auth/refresh', TokenRefreshView.as_view(), name="token_refresh")
 ]
--- chatbot/authentication/views.py
+++ chatbot/authentication/views.py
@@ -2,12 +2,15 @@ from authentication.serializers import UsersSerializer
 from authentication.models import User
 from rest_framework import generics, permissions
 from .permissions import UserPermissions
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-class AuthUser(generics.CreateAPIView):
-  serializer_class = UsersSerializer
+class MyTokenView(TokenObtainPairView):
+  permission_classes = []
 
 class PostUser(generics.CreateAPIView):
   serializer_class = UsersSerializer
+  authentication_classes = []
+  permission_classes = []
 
 class RetrieveDeleteUser(generics.RetrieveDestroyAPIView):
   queryset = User.objects.all()
