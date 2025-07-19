2af1599fbf775741e9343590aae61e6363734108
CSamu7 <samuel.pdg@hotmail.com>
2025-07-18 22:29:31 -0600

    Se añadio tests para la creación de usuario, permisos y token

--- chatbot/authentication/tests/tests_views.py
+++ chatbot/authentication/tests/tests_views.py
@@ -1,13 +1,43 @@
-from rest_framework.test import APIRequestFactory
+from rest_framework.test import APIRequestFactory, APIClient, force_authenticate
 from django.test import TestCase
 from ..models import User
+from ..views import RetrieveDeleteUser
+import json
 
-# Create your tests here.
 class AuthViewTestCase(TestCase):
-  factory = APIRequestFactory()
+  request_factory = APIRequestFactory()
+  api_client = APIClient()
+  raw_data_user = json.dumps({
+    "email": "inventado@gmail.com",
+    "password": "123"                                                                      
+  })
     
   def setUp(self):
-    User.objects.create(username="samu", password='123', email="inventado@gmail.com")
+    User.objects.create_user(username="samu", password="123", email="inventado@gmail.com")
 
-  def test_return_token_if_user_exists(self):
-    self.factory.get()
+  def test_return_token_if_db_user_exists(self):
+    response = self.client.post('/api/users/auth', data=self.raw_data_user, content_type="application/json")
+    self.assertIs(response.status_code, 200)
+
+  def test_create_new_user(self):
+    data = json.dumps({
+      "username": "soynuevo",
+      "email": "soynuevo@gmail.com",
+      "password": "1234"
+    })
+
+    response = self.client.post('/api/users/', data=data, content_type="application/json")
+    self.assertIs(response.status_code, 201)
+
+    user_created = User.objects.get(email="soynuevo@gmail.com")
+    self.assertIsNotNone(user_created)
+
+  def test_get_details_user_if_user_is_authenticated(self):
+    user = User.objects.get(email="inventado@gmail.com")
+    request = self.request_factory.get(f"/api/users/{user.pk}")
+    force_authenticate(request, user=user)
+    view = RetrieveDeleteUser().as_view()
+
+    response = view(request, pk=user.pk)
+    self.assertIs(response.status_code, 200)
+  
